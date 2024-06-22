3bf76db8a1e6560c9300522246d3467ab1232f99
mur1chan <126001665+mur1chan@users.noreply.github.com>
2024-06-23 01:06:27 +0200

    htmx swap, save button
    added a htmx swap function in the dashboard for a more app like feeling and a save button for the next htmx swap and display function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use actix_files::Files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use askama::Template;
-use actix_files::Files;
+use actix_htmx::{Htmx, HtmxMiddleware, TriggerType};
 
 
 #[derive(Template)]
@@ -12,35 +13,50 @@ struct CardTemplate<'a> {
 #[template(path = "editor.html")]
 struct EditorTemplate;
 
-
 #[derive(Template)]
 #[template(path = "dashboard.html")]
 struct DashboardTemplate<'a> {
     cards: &'a str,
 }
 
-async fn hello() -> impl Responder {
-    let titles = vec!["Blog Entry 1", "Blog Entry 2", "Blog Entry 3", "Blog Entry 4", "Blog Entry 5"];
+async fn hello(htmx: Htmx) -> impl Responder {
+    if htmx.is_htmx {
+        println!("{:?}", htmx.target())
+    }
+    let titles = vec![
+        "Blog Entry 1",
+        "Blog Entry 2",
+        "Blog Entry 3",
+        "Blog Entry 4",
+        "Blog Entry 5",
+    ];
     let mut cards_html = String::new();
-
+    
     for title in titles {
         let card = CardTemplate { card_title: title };
         cards_html.push_str(&card.render().unwrap());
     }
 
     let base = DashboardTemplate { cards: &cards_html };
-    HttpResponse::Ok().content_type("text/html").body(base.render().unwrap())
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(base.render().unwrap())
 }
 
 async fn editor() -> impl Responder {
     let editor = EditorTemplate;
-    HttpResponse::Ok().content_type("text/html").body(editor.render().unwrap())
+    
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(editor.render().unwrap())
+        
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .wrap(HtmxMiddleware)
             .service(Files::new("/static", "./static"))
             .route("/", web::get().to(hello))
             .route("/editor", web::get().to(editor))
