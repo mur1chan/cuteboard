76ecfe85ca495257f00e9ec487f5ccc3c3a55880
mur1chan <126001665+mur1chan@users.noreply.github.com>
2024-06-23 03:30:36 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -83,7 +83,7 @@ async fn main() -> std::io::Result<()> {
             .route("/editor", web::get().to(editor))
             .route("/receive-body", web::post().to(receive_body))
     })
-    .bind("127.0.0.1:1337")?
+    .bind("127.0.0.1:8000")?
     .run()
     .await
 }
